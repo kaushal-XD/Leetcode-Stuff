fde88ab4cfe6aacd54d175d80f58c8cc34a2acc9
Kaushal Landge <er.kaushallandge@gmail.com>
2026-07-16 09:57:10 +0530

    Time: 79 ms (16.53%), Space: 21.7 MB (18.04%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = 1e9 ;
+        vector<int> dp(n,maxi);
+        dp[0] = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i+1;j<=min(n-1,nums[i]+i);j++){
+                dp[j] = min(dp[j] , dp[i]+1);
+            }
+        }
+        return dp[n-1];
+
+
+
+    }
+};
