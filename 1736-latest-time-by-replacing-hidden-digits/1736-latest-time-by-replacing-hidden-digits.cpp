80bb4c1a75580714e9ededd43e86a94a553699bc
Kaushal Landge <er.kaushallandge@gmail.com>
2026-08-15 15:36:30 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.4%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string maximumTime(string time) {
+        if (time[0] == '?' && time[1] == '?') {
+            time[0] = '2';
+            time[1] = '3';
+        }
+        if (time[0] == '?') {
+            char ch = time[1] - '0';
+            if (ch <= 3)
+                time[0] = '2';
+            else
+                time[0] = '1';
+        }
+        if (time[1] == '?') {
+            char ch = time[0] - '0';
+            if (ch == 2)
+                time[1] = '3';
+            else
+                time[1] = '9';
+        }
+
+        if (time[3] == '?')
+            time[3] = '5';
+        if (time[4] == '?')
+            time[4] = '9';
+
+        return time;
+    }
+};
