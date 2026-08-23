b5099313342e8a09df917d2e1f003049cc164ff6
Kaushal Landge <er.kaushallandge@gmail.com>
2026-08-23 20:06:57 +0530

    Time: 1554 ms (18.73%), Space: 420.9 MB (30.17%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7 ;
-    vector<vector<vector<int>>> memo;
 
-    int func(int n, int a , int l){
-        if(a >= 2 || l >= 3) return 0;
-        if (n == 0) return 1;
-
-        if (memo[n][a][l] != -1) return memo[n][a][l];
-
-
-        int ans = func(n-1,a,0)%mod;
-        ans = (ans+func(n-1,a+1,0))%mod;
-        ans = (ans + func(n-1,a,l+1))%mod;
-        memo[n][a][l] = ans;
-        return ans;
-    }
     int checkRecord(int n) {
-        memo.assign(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return func(n,0,0);
-        
+        const int mod = 1e9 + 7 ;
+        vector<vector<int>> dp(2, vector<int>(3,0));
+        dp[0][0] = 1;
+
+        for(int i = 1 ; i <= n ; i++){
+            vector<vector<int>> tempdp(2, vector<int>(3,0));
+            for(int a = 0 ; a < 2 ; a++){
+                for(int l = 0 ; l < 3 ; l++){
+                    if(dp[a][l] == 0) continue;
+
+                    tempdp[a][0] = (tempdp[a][0] + dp[a][l])%mod;
+
+                    if (a +1 < 2 ){
+                        tempdp[a+1][0] = (tempdp[a+1][0] + dp[a][l])%mod;
+                    }
+
+                    if (l + 1 < 3){
+                        tempdp[a][l+1] = (tempdp[a][l+1] + dp[a][l])%mod;
+                    }
+                }
+            }
+            dp = move(tempdp);
+        }
+        int ans = 0;
+        for(int a = 0 ; a < 2 ; a++){
+            for(int l = 0 ; l < 3 ; l++){
+                ans = (ans + dp[a][l])%mod;
+            }
+        }
+        return ans;
     }
 };
