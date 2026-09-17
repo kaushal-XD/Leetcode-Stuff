37d42e48c7c09ca7b30d4241664ec580cb2c5460
Kaushal Landge <er.kaushallandge@gmail.com>
2026-09-17 21:03:53 +0530

    Time: 0 ms (100%), Space: 8 MB (37.66%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
-    long long pow(long long num, long long p, long long m){
+    long long pow(long long num, long long p){
+        long long m = 1e9+7;
         if (p == 0) return 1;
-        long long temp = pow(num,p/2,m)%m;
-        if (p%2==0) return (temp*temp)%m;
-        else return ((num%m)*(temp)*(temp))%m;
+        long long res = 1;
+        num%=m;
+
+        while(p > 0){
+            if(p&1) res = (res*num)%m;
+            num = (num*num)%m;
+            p>>=1;
+        }
+        return res%m;
+        
     }
     int countGoodNumbers(long long n) {
         long long m = 1e9+7;
-        if (n%2 == 0) return (pow(5,n/2,m)*pow(4,n/2,m))%m;
-        else return (pow(5,n/2 +1,m)*pow(4,n/2,m))%m;
-
+        if (n%2 == 0) return (pow(5,n/2)*pow(4,n/2))%m;
+        else return (pow(5,n/2 +1)*pow(4,n/2))%m;
     }
 };
